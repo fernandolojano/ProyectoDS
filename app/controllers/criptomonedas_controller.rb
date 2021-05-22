35bc296279a8d8dd532ddc69a73290c7fdfbf1ee
mnc99 <moises.noguera1999@gmail.com>
2021-05-22 18:15:14 +0200

    Updates id criptmoneda

--- app/controllers/criptomonedas_controller.rb
+++ app/controllers/criptomonedas_controller.rb
@@ -41,7 +41,7 @@ class CriptomonedasController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_criptomoneda
-      @criptomoneda = Criptomoneda.find(params[:id])
+      @criptomoneda = Criptomoneda.find(params[:nombre])
     end
 
     # Only allow a list of trusted parameters through.
