358fbd2e3d38e583c4bfa85afa8adec7134a5655
GFernando97 <e.fernandolojano@go.ugr.es>
2021-05-22 20:16:22 +0200

    re: config rutas

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format:'json'} do
   	namespace :v1 do
-  		resources :criptomonedas
+  		resources :balance_divisas
+      resources :balance_criptomonedas
+      resources :valor_historicos
+      resources :criptomonedas
+      resources :divisas
+      resources :usuarios
   	end
   end
 end
