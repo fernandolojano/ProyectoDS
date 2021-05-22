9657d35c3281bdbff3d291a70de2d6b51ea643fd
GFernando97 <e.fernandolojano@go.ugr.es>
2021-05-22 20:42:29 +0200

    re: vistas

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,2 +1,8 @@
 class Usuario < ApplicationRecord
+	validates :username, presence: true
+    validates :nombre, presence: true
+    validates :password, presence: true
+    validates_uniqueness_of :username
+    validates_confirmation_of :password, :on => :create
+    validates_length_of :password, :within => 8..20
 end
--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module DS14
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Madrid"
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
