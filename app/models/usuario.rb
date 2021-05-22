9ab97149e6c03df2e237c07090d1cc9c8dafcbff
mnc99 <moises.noguera1999@gmail.com>
2021-05-22 17:56:54 +0200

    Cambio name a nombre en usuario modelo

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,6 +1,6 @@
 class Usuario < ApplicationRecord
     validates :username, presence: true
-    validates :name, presence: true
+    validates :nombre, presence: true
     validates :password, presence: true
     validates_uniqueness_of :username
     validates_confirmation_of :password, :on => :create
