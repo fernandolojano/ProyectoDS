09f50e6208aa76e33581c512b9dc09f5c92e4bd0
Miguelangel Valderrama <miguelangelx.72@gmail.com>
2021-05-23 17:52:58 +0200

    API: test join 5

--- app/controllers/api/v1/balance_criptomonedas_controller.rb
+++ app/controllers/api/v1/balance_criptomonedas_controller.rb
@@ -6,9 +6,7 @@ class BalanceCriptomonedasController < ApplicationController
 
   # GET /balance_criptomonedas
   def index
-    @balance_criptomonedas = BalanceCriptomoneda.all.joins
-    ("LEFT JOIN criptomonedas ON token_criptomoneda")
-    .order('valor DESC')
+    @balance_criptomonedas = BalanceCriptomoneda.all.joins("LEFT JOIN criptomonedas ON token_criptomoneda").order('valor DESC')
 
     render json: @balance_criptomonedas
   end
