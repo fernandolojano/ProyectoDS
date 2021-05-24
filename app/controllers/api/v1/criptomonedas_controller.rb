c7bcebcfcb810841bfaaa402c80014ac4e2ebb71
Miguelangel Valderrama <miguelangelx.72@gmail.com>
2021-05-24 09:19:10 +0200

    API: criptomendas por clave primaria

--- app/controllers/api/v1/criptomonedas_controller.rb
+++ app/controllers/api/v1/criptomonedas_controller.rb
@@ -48,7 +48,7 @@ class CriptomonedasController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_criptomoneda
-      @criptomoneda = Criptomoneda.find_by(nombre: params[:id])
+      @criptomoneda = Criptomoneda.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
