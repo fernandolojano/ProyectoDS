e59aab16df5cef4158366032086bbecc6c096392
mnc99 <moises.noguera1999@gmail.com>
2021-05-30 17:36:52 +0200

    Test usuario y divisa

--- test/models/divisa_test.rb
+++ test/models/divisa_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class DivisaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Caso de prueba divisa 1" do
+    # Primer caso: Divisa que ya pertenece a la base de datos
+    divisa = Divisa.new(token_divisa:"USD", nombre_divisa:"Pesetas", valor:200, simbolo:"P")
+    assert_not divisa.save, "No se ha guardado el usuario porque ya existe"
+  end
+
+  test "Caso de prueba divisa 2" do
+    # Segundo caso: Divisa con un valor negativo
+    divisa = Divisa.new(token_divisa:"PES", nombre_divisa:"Pesetas", valor:-12, simbolo:"P")
+    assert_not divisa.save, "No se ha guardado el usuario porque la password es muy corta"
+  end
+      
+  test "Caso de prueba divisa 3" do
+    # Tercer caso: Nueva divisa con parámetros correctos
+    divisa = Divisa.new(token_divisa:"PES", nombre_divisa:"Pesetas", valor:200, simbolo:"P")
+    assert divisa.save, "El usuario se ha guardado correctamente"
+  end
 end
--- test/models/usuario_test.rb
+++ test/models/usuario_test.rb
@@ -2,25 +2,22 @@ require "test_helper"
 
 class UsuarioTest < ActiveSupport::TestCase
 
-
-   test "Password should be longer than 8 characters" do
-   	 usuario = Usuario.new	
-     assert_not usuario.save, "Saved usuario with Password length equals < 8"
+   test "Caso de prueba usuario 1" do
+      # Primer caso: Usuario que ya pertenece a la base de datos
+   	usuario = Usuario.new(username:"mnc99", nombre:"Moises", password:"12345678")
+      assert_not usuario.save, "No se ha guardado el usuario porque ya existe"
    end
 
-   test ".length returns a number bigger or equal than 8" do
-
-      # Primer caso
-   	usuario = Usuario.new(username:"GFernando", nombre:"Fernando", password:"12")
-   	assert_equal true,usuario.password.length >= 8
-
-      # Segundo caso
+   test "Caso de prueba usuario 2" do
+      # Segundo caso: Nuevo usuario con contraseña no válida
       usuario = Usuario.new(username:"GFernando", nombre:"Fernando", password:"12")
-   	assert_equal true,usuario.password.length >= 8
-
-      # Tercer caso
-      usuario = Usuario.new(username:"GFernando", nombre:"Fernando", password:"12")
-   	assert_equal true,usuario.password.length >= 8
+   	assert_not usuario.save, "No se ha guardado el usuario porque la password es muy corta"
+   end
+      
+   test "Caso de prueba usuario 3" do
+      # Tercer caso: Nuevo usuario con parámetros correctos
+      usuario = Usuario.new(username:"GFernando", nombre:"Fernando", password:"12345678")
+   	assert usuario.save, "El usuario se ha guardado correctamente"
    end
 
 end
